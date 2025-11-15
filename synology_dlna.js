5c311a2c482940bf2996f44107127f015671ad1b
she4ki <she4ki@gmail.com>
2025-11-15 16:31:11 +0300

    Update synology_dlna.js

--- synology_dlna.js
+++ synology_dlna.js
@@ -20,7 +20,7 @@
           body.append(scroll.render(true));
           tree = {
             device: {name: server},
-            tree: [{title:"/", id: 0}]
+            tree: [{title:"/video", id: 1}]
           };
           this.displayFolder();
         } else {
